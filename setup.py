751f38d4ea6335efb21d1cb409436c5b6e136602
Tim Webster <twebster17@gmail.com>
2020-06-21 22:01:49 -0600

    set up for command line execution

--- setup.py
+++ setup.py
@@ -12,5 +12,11 @@ setup(
     author='tinalasisi',
     author_email='tpl5158@psu.edu',
     description='Toolkit for analyzing hair fiber morphology',
-    install_requires=['numpy', 'scipy', 'matplotlib', 'joblib', 'pandas', 'cv2', 'scikit-learn', 'PIL', 'rawpy']
+    install_requires=[
+        'numpy', 'scipy', 'matplotlib', 'joblib', 'pandas', 'opencv-python',
+        'scikit-learn', 'Pillow', 'rawpy', 'requests', 'sympy', 'argparse',
+        'scikit-image', 'joblib', 'matplotlib'],
+    entry_points={
+        "console_scripts": [
+            'fibermorph = fibermorph.fibermorph:main']}
 )
